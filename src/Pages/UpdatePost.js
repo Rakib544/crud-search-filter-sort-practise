039aa054d35226aa226ec0a482b37dee149af6df
Rakib544 <md.rakib10122003@gmail.com>
2021-06-28 17:15:16 +0600

    update post functionality done

--- src/Pages/UpdatePost.js
+++ src/Pages/UpdatePost.js
@@ -1,7 +1,18 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { UserContext } from '../App';
 
 const UpdatePost = () => {
+
+    const { id } = useParams();
+
+    //Get context api data
+    const [posts, setPosts] = useContext(UserContext)
+    // console.log(posts)
     const [updatedInfo, setUpdatedInfo] = useState({ title: "", body: "" })
+
+    // FInd the post which will be updating
+    const updatingPostIndex = posts.findIndex(post => post.id === parseInt(id));
 
     const handleChange = e => {
         setUpdatedInfo({
@@ -12,28 +23,34 @@ const UpdatePost = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault()
+        setPosts([
+            ...posts,
+            posts[updatingPostIndex].title = updatedInfo.title,
+            posts[updatingPostIndex].body = updatedInfo.body
+        ])
     }
 
-    // const handleUpdatePost = id => {
-    //     fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
-    //         method: 'PATCH',
-    //         body: JSON.stringify({
-    //             title: 'foo',
-    //         }),
-    //         headers: {
-    //             'Content-type': 'application/json; charset=UTF-8',
-    //         },
-    //     })
-    //         .then((response) => response.json())
-    //         .then((json) => console.log(json));
-    // }
-
     return (
-        <div>
-            <form onSubmit={handleSubmit} className="pt-24">
-                <input name="title" onChange={handleChange} type="text" placeholder="title" />
-                <input name="body" onChange={handleChange} type="text" placeholder="body" />
-                <input type="submit" value="Update" />
+        <div className="flex justify-center items-center h-screen">
+            <form
+                onSubmit={handleSubmit}
+                className="w-full md:w-9/12 lg:w-1/2"
+            >
+                <input
+                    name="title"
+                    onChange={handleChange}
+                    type="text"
+                    placeholder="title"
+                    className="w-full rounded-full mb-4 py-3 px-8 ring-0 focus:outline-none border border-gray-700"
+                />
+                <input
+                    name="body"
+                    onChange={handleChange}
+                    type="text"
+                    placeholder="body"
+                    className="w-full rounded-full mb-4 py-3 px-8 ring-0 focus:outline-none border border-gray-700"
+                />
+                <button type="submit" className="btn">Update</button>
             </form>
         </div>
     );
